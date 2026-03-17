c6f728aad9486aa6fe0fa5494bcc01f00a07bcd8
ishreyasp <purkarshreyas@gmail.com>
2026-03-16 23:20:30 -0400

    add javadoc

--- arrays/LongestCommonPrefix.java
+++ arrays/LongestCommonPrefix.java
@@ -1,3 +1,19 @@
+/**
+You are given an array of strings strs. Return the longest common prefix of all the strings.
+If there is no longest common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["bat","bag","bank","band"]
+Output: "ba"
+
+Example 2:
+Input: strs = ["dance","dag","danger","damage"]
+Output: "da"
+
+Example 3:
+Input: strs = ["neet","feet"]
+Output: ""
+*/
 class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         if(strs==null || strs.length==0){
