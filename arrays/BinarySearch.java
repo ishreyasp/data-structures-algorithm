1806ea338340ba06f994107a7e1edc64f0804821
ishreyasp <purkarshreyas@gmail.com>
2026-04-17 20:34:50 -0400

    add binary search code

---
+++ arrays/BinarySearch.java
@@ -0,0 +1,40 @@
+/**
+You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+
+Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+
+Your solution must run in O(logn) time.
+
+Example 1:
+Input: nums = [-1,0,2,4,6,8], target = 4
+Output: 3
+ */
+class BinarySearch {
+
+    public static int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
+        int result = search(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
