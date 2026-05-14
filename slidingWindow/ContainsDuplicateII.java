714945f6febbb9833561600fbcba09ec92c10848
ishreyasp <purkarshreyas@gmail.com>
2026-05-14 14:31:19 -0400

    add contains duplicate code

--- slidingWindow/ContainsDuplicateII.java
+++ slidingWindow/ContainsDuplicateII.java
@@ -1,3 +1,15 @@
+/**
+You are given an integer array nums and an integer k, return true if there are two distinct indices i and j 
+in the array such that nums[i] == nums[j] and abs(i - j) <= k, otherwise return false.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [2,1,2], k = 1
+Output: false
+ */
 import java.util.HashSet;
 import java.util.Set;
 
