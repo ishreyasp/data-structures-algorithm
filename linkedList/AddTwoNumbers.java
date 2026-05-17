c2fc36be52e28d15c0ac9b73e08e5b2f56c2045e
ishreyasp <purkarshreyas@gmail.com>
2026-05-17 12:44:25 -0400

    add two linkedlist code

---
+++ linkedList/AddTwoNumbers.java
@@ -0,0 +1,73 @@
+/**
+You are given two non-empty linked lists, l1 and l2, where each represents a non-negative integer.
+
+The digits are stored in reverse order, e.g. the number 321 is represented as 1 -> 2 -> 3 -> in the linked list.
+
+Each of the nodes contains a single digit. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Return the sum of the two numbers as a linked list.
+
+Example 1:
+Input: l1 = [1,2,3], l2 = [4,5,6]
+Output: [5,7,9]
+Explanation: 321 + 654 = 975.
+
+Example 2:
+Input: l1 = [9], l2 = [9]
+Output: [8,1]
+ */
+class AddTwoNumbers {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            carry = sum / 10;
+        }   
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        AddTwoNumbers solution = new AddTwoNumbers();
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(4);
+        l2.next = new ListNode(5);
+        l2.next.next = new ListNode(6);
+
+        ListNode result = solution.addTwoNumbers(l1, l2);
+        while(result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
