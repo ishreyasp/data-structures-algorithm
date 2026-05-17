ecd6ef8d4b40044a24bda58976eb421feb86e8fe
ishreyasp <purkarshreyas@gmail.com>
2026-05-17 11:01:10 -0400

    add merge linkedlist code

---
+++ linkedList/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,80 @@
+/**
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+
+The new list should be made up of nodes from list1 and list2.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,5]
+Output: [1,1,2,3,4,5]
+
+Example 2:
+Input: list1 = [], list2 = [1,2]
+Output: [1,2]
+
+Example 3:
+Input: list1 = [], list2 = []
+Output: []
+ */
+public class MergeTwoSortedLinkedLists {
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if(list1 != null) {
+            curr.next = list1;
+        }
+
+        if(list2 != null) {
+            curr.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        MergeTwoSortedLinkedLists solution = new MergeTwoSortedLinkedLists();
+
+        // Example usage:
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode mergedHead = solution.mergeTwoLists(list1, list2);
+
+        // Print merged list
+        ListNode current = mergedHead;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+    
+}
