3f3a0a555e569d1f4536592003c8235ad3d1dd7d
ishreyasp <purkarshreyas@gmail.com>
2026-05-17 16:08:06 -0400

    add numbers disappeared code

--- negativeMarking/NumbersDisappearedInArray.java
+++ negativeMarking/NumbersDisappearedInArray.java
@@ -15,7 +15,7 @@ Example 2:
 Input: nums = [1,1]
 Output: [2]
  */
-class NumbersDisapperedInArray {
+class NumbersDisappearedInArray {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         for(int num : nums) {
             int i = Math.abs(num) - 1;
@@ -32,7 +32,7 @@ class NumbersDisapperedInArray {
     }
 
     public static void main(String[] args) {
-        NumbersDisapperedInArray obj = new NumbersDisapperedInArray();
+        NumbersDisappearedInArray obj = new NumbersDisappearedInArray();
         int[] nums = {4,3,2,7,8,2,3,1};
         System.out.println(obj.findDisappearedNumbers(nums));
     }
